7e9671b3f918548e2fea57538dbd8a0d5da21d48
alper ademoglu <alperademoglu@hotmail.com>
2022-08-30 19:25:26 -0400

    Log in and logout now work

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = "recipes_list"
+LOGOUT_REDIRECT_URL = "recipes_list"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
